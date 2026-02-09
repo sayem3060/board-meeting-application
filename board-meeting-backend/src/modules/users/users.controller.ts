3c80cc6ec2ede9863e35f7f27d4d52631f04c101
sayem3060 <sayem@nazihargroup.com>
2026-02-09 17:43:57 +0600

    modify user controller with jwtauth guard

--- board-meeting-backend/src/modules/users/users.controller.ts
+++ board-meeting-backend/src/modules/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
@@ -11,32 +13,38 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: Partial<CreateUserDto>) {
     return this.usersService.update(id, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('by-email/:email')
   findByEmail(@Param('email') email: string) {
     const emailArray = email.split(',');
     return this.usersService.findByEmail(email);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('by-phone-number/:phoneNumber')
   findByPhoneNumber(@Param('phoneNumber') phoneNumber: string) {
     const phoneNumberArray = phoneNumber.split(',');
